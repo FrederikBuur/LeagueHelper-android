c1ee4da77f1d9504d28ec1e0994bb09114826cd1
Frederik Buur <frederik_buur@hotmail.com>
2019-08-30 10:05:16 +0200

    Added binding view switcher

---
+++ app/src/main/java/com/example/leaguehelper/util/viewswitcher/BindingViewSwitcher.kt
@@ -0,0 +1,61 @@
+package com.example.leaguehelper.util.viewswitcher
+
+import android.content.Context
+import android.os.Handler
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ViewSwitcher
+import me.tatarka.bindingcollectionadapter2.ItemBinding
+
+class BindingViewSwitcher : ViewSwitcher {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+
+    var itemBinding: ItemBinding<Any>? = null
+        set(value) {
+            if (field == null) {
+                field = value
+                value?.let { itemBinding ->
+                    adapter = ViewSwitcherAdapter(itemBinding)
+                }
+            }
+        }
+
+    var item: Any? = null
+        set(value) {
+            field = value
+            adapter?.item = item
+        }
+
+    private var adapter: ViewSwitcherAdapter<Any>? = null
+        set(value) {
+            field = value
+            value?.let { adapter ->
+                adapter.attach(
+                    onSwitchView = { view ->
+                        switchView(view = view)
+                    },
+                    context = context)
+                adapter.item = item
+            }
+        }
+
+    private fun switchView(view: View?) {
+        currentView?.let { currentView ->
+            if (view != null) {
+                addView(view)
+                showNext()
+            }
+
+            Handler().post {
+                removeView(currentView)
+            }
+        } ?: {
+            if (view != null) {
+                addView(view)
+            }
+        }.invoke()
+    }
+}
---
+++ app/src/main/java/com/example/leaguehelper/util/viewswitcher/ViewSwitcherAdapter.kt
@@ -0,0 +1,90 @@
+package com.example.leaguehelper.util.viewswitcher
+
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import android.view.LayoutInflater
+import android.view.View
+import android.content.Context
+import me.tatarka.bindingcollectionadapter2.ItemBinding
+
+class ViewSwitcherAdapter<T>(private val itemBinding: ItemBinding<T>) {
+
+    private var currentBinding: ViewDataBinding? = null
+    private var compareString: String? = null
+
+    fun attach(context: Context, onSwitchView: (view: View?) -> (Unit)) {
+        this.context = context
+        this.onSwitchView = onSwitchView
+
+        trySwitchView()
+    }
+
+    var item: T? = null
+        set(value) {
+            synchronized(this) {
+                field = value
+                trySwitchView()
+            }
+        }
+
+    private var context: Context? = null
+
+    private var onSwitchView: ((view: View?) -> (Unit))? = null
+
+    private fun trySwitchView() {
+        context?.let { context ->
+            onSwitchView?.let { onSwitchView ->
+                item?.let { item ->
+                    switchView(
+                        itemBinding = itemBinding,
+                        item = item,
+                        context = context,
+                        onSwitchView = onSwitchView
+                    )
+                } ?: clearView(onSwitchView)
+            }
+        }
+    }
+
+    private fun clearView(onSwitchView: (view: View?) -> Unit) {
+        itemBinding.set(0, 0)
+        currentBinding = null
+        onSwitchView.invoke(null)
+    }
+
+    private fun switchView(
+        itemBinding: ItemBinding<T>,
+        item: T,
+        context: Context,
+        onSwitchView: (view: View?) -> Unit
+    ) {
+        val currentlayoutRes = itemBinding.layoutRes()
+        itemBinding.onItemBind(0, item)
+
+        var inflate = currentlayoutRes != itemBinding.layoutRes()
+        var bind = true
+        if (!inflate) {
+            if (item.toString() != compareString) {
+                inflate = true
+                bind = true
+            }
+        }
+
+        if (inflate) {
+            currentBinding = DataBindingUtil.inflate(LayoutInflater.from(context), itemBinding.layoutRes(), null, false)
+        }
+
+        currentBinding?.let { binding ->
+            if (bind) {
+                binding.setVariable(itemBinding.variableId(), item)
+                binding.executePendingBindings()
+            }
+
+            if (inflate) {
+                onSwitchView.invoke(binding.root)
+            }
+        }
+
+        compareString = item.toString()
+    }
+}
